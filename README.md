859c5b7c14041441d3539dd3ea75a6fa9791d166
Eric Dalongeville <22616866+edalongeville@users.noreply.github.com>
2020-01-30 23:11:19 +0100

    Initial code commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# Battlescribe_Ubuntu_install_script
-This script properly installs Battlescribe on Ubuntu 18.04
+# Battlescribe_install_script
+This script will install Battlescribe (with icons and .desktop link) on Ubuntu 18.04
+
+## This is intended to be used on Ubuntu 18.04.
+
+# What it does
+- Deploys Battlescribe with your other apps (in /usr/bin)
+- Creates a .desktop file, so you can access Battlescribe from Gnome, like any other app
+- Deploys the icon, required for the above .desktop link
+
+# Instructions
+1. Download/Clone this repo
+4. Run the install script (**root access will be required**). If required, add exec permission with *chmod +x*.
+Note: You may have to restart your session for the icon to be properly displayed in gnome
---
+++ battlescribe_install_Ubuntu.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# This script installs Battlescribe on Ubuntu 18.04
+
+FILE="BattleScribe_2.03.11_Multi.zip"
+ICON="battlescribe_icon_128.png"
+DESKFILE="battlescribe.desktop"
+INSTALL_DIR="/usr/bin/battlescribe"
+ICONS_DIR="/usr/share/icons/hicolor"
+DESKFILES_DIR="/usr/share/applications"
+
+# Download Battlescribe
+wget http://battlescribe.net/files/$FILE
+
+# Download Icon
+wget https://www.battlescribe.net/images/icon/$ICON
+
+# Extract Battlescribe to install dir
+sudo mkdir $INSTALL_DIR
+sudo unzip $FILE -d $INSTALL_DIR
+
+# Create run script
+echo "#!/bin/bash" | sudo tee -a $INSTALL_DIR/Battlescribe.sh  >/dev/null
+echo "java -jar $INSTALL_DIR/RosterEditor.jar" | sudo tee -a $INSTALL_DIR/Battlescribe.sh  >/dev/null
+
+# Make file executable
+sudo chmod +x $INSTALL_DIR/Battlescribe.sh
+
+# Deploy icons
+## 32x32
+sudo convert -resize 32x32 $ICON $ICONS_DIR/32x32/apps/$ICON
+## 48x48
+sudo convert -resize 48x48 $ICON $ICONS_DIR/48x48/apps/$ICON
+## 64x64
+sudo convert -resize 64x64 $ICON $ICONS_DIR/64x64/apps/$ICON
+## 128x128
+sudo cp $ICON $ICONS_DIR/128x128/apps/$ICON
+
+# Deploy desktop file (Gnome menu entry)
+sudo cp $DESKFILE $DESKFILES_DIR/$DESKFILE
+
+# Cleanup
+rm $FILE
+rm $ICON
