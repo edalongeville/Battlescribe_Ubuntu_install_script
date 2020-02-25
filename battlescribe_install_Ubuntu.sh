2e2cbda2ea391ee8e820e39dd5af4af2baa38e8b
Eric Dalongeville <22616866+edalongeville@users.noreply.github.com>
2020-02-25 13:29:29 +0100

    Version bump

--- battlescribe_install_Ubuntu.sh
+++ battlescribe_install_Ubuntu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This script installs Battlescribe on Ubuntu 18.04
 
-FILE="BattleScribe_2.03.11_Multi.zip"
+FILE="BattleScribe_2.03.12_Multi.zip"
 ICON_WEB="battlescribe_icon_128.png"
 ICON="battlescribe.png"
 DESKFILE="battlescribe.desktop"
